af7c884f4a6d3205081c0597ded4c48dc6b2f250
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-01-09 09:06:34 +0100

    Include path en MobileDatabaseHandler class
    * Stel het include path goed in voor alle PHP scripts.
    * MobileDatabaseHandler voor querys voor mobiele app

---
+++ classes/MobileDatabaseHandler.class.php
@@ -0,0 +1,15 @@
+<?php
+// Imports
+require_once 'classes/DatabaseHandler.class.php';
+
+/**
+ * Verzorgt alle verbindingen naar de database toe voor de mobiele app.
+ *
+ * @name Mobile DatabaseHandler      
+ * @author Sven Dubbeld <sven.dubbeld1@gmail.com>
+ *        
+ */
+class MobileDatabaseHandler extends DatabaseHandler {
+}
+
+?>
