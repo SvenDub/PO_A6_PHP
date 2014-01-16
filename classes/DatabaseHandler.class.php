7835b4b9b5f16d7b7a409e4e538477f5290d3b55
StefanPeeman <stefan_peeman@msn.com>
2014-01-16 09:25:19 +0100

    Update DatabaseHandler.class.php
    Een aantal query's gemaakt

--- classes/DatabaseHandler.class.php
+++ classes/DatabaseHandler.class.php
@@ -171,9 +171,219 @@ class DatabaseHandler {
 		$stmt->close ();
 	}
 	
-	function klant_toevoegen($nummer, $id, $tafelnummer, $aantal_klanten, $actief, $datum) {
+	function klant_toevoegen( $id, $tafelnummer, $aantal_klanten, $actief, $datum) {
 		// De te gebruiken query
-		$query = "INSERT INTO producten ( nummer, id, tafelnummer, aantal_klanten, actief, datum )  VALUES ( ?, ?, ?, ? ) ";
+		$query = "INSERT INTO tafelregistratie (  id, tafelnummer, aantal_klanten, actief, datum )  VALUES (  ?, ?, ?, ?, ? ) ";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'iiiis',  $id, $tafelnummer, $aantal_klanten, $actief, $datum )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+	function tafelnummer_toevoegen($tafelnummer) {
+		// De te gebruiken query
+		$query = "INSERT INTO tafelnummer ( tafelnummer )  VALUES ( ? ) ";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'i', $tafelnummer )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+	
+	function categorie_toevoegen( $categorienummer, $categorie) {
+		// De te gebruiken query
+		$query = "INSERT INTO categorie ( categorienummer, categorie )  VALUES ( ?, ? ) ";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'is', $categorienummer, $categorie )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+	
+	function bestelling_toevoegen($nummer, $id, $productcode, $aantal_besteld, $opmerking, $datum) {
+		// De te gebruiken query
+		$query = "INSERT INTO bestellingen ( nummer, id, productcode, aantal_besteld, opmerking, datum )  VALUES ( ?, ?, ?, ?, ?, ? ) ";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'iiiiss', $nummer, $id, $productcode, $aantal_besteld, $opmerking, $datum )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+	function personeel_wijzigen($id, $gebruikersnaam, $wachtwoord, $beheer, $actief) {
+		// De te gebruiken query
+		$query = " UPDATE inlogsysteem ( id, gebruikersnaam, wachtwoord, beheer, actief )  VALUES ( ?, ?, ?, ?, ? ) 
+		           WHERE id=?";
+		             
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'issii', $id, $gebruikersnaam, $wachtwoord, $beheer, $actief )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+
+                 function product_wijzigen($productcode, $categorie, $gerecht, $prijs) {
+		// De te gebruiken query
+		$query = "UPDATE producten ( productcode, categorie, geerechtrecht, prijs )  VALUES ( ?, ?, ?, ? ) 
+		          WHERE productcode=?";
 		
 		// Maak een nieuw statement
 		$stmt = $this->con->stmt_init ();
@@ -208,6 +418,172 @@ class DatabaseHandler {
 		// Sluit het statement om geheugen vrij te geven
 		$stmt->close ();
 	}
+		function klant_wijzigen( $nummer, $id, $tafelnummer, $aantal_klanten, $actief, $datum) {
+		// De te gebruiken query
+		$query = "UPDATE tafelregistratie ( id, tafelnummer, aantal_klanten, actief, datum )  VALUES (  ?, ?, ?, ?, ? ) 
+		          WHERE nummer=?";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'iiiisi',  $id, $tafelnummer, $aantal_klanten, $actief, $datum, $nummer)) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
 	
+		$stmt->close ();
+		
+		
+		
+	}
+                function tafelnummer_wijzigen($tafelnummer) {
+		// De te gebruiken query
+		$query = "UPDATE tafelnummer ( tafelnummer )  VALUES ( ? ) 
+		          WHERE tafelnummer=?";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'i', $tafelnummer )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
 	
+		$stmt->close ();
+		
+		
+		
+	}
+         	function categorie_wijzigen( $categorienummer, $categorie) {
+		// De te gebruiken query
+		$query = "UPDATE categorie ( categorienummer, categorie )  VALUES ( ?, ? )
+		          WHERE categorienummer=?";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'is', $categorienummer, $categorie )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
+                function bestelling_wijzigen ($nummer, $id, $productcode, $aantal_besteld, $opmerking, $datum, $bestellingnummer) {
+		// De te gebruiken query
+		$query = "INSERT INTO bestellingen ( nummer, id, productcode, aantal_besteld, opmerking, datum )  VALUES ( ?, ?, ?, ?, ?, ? ) 
+		          WHERE bestellingnummer=?";
+		
+		// Maak een nieuw statement
+		$stmt = $this->con->stmt_init ();
+		
+		// Bereid de query voor
+		if ($stmt->prepare ( $query )) {
+			
+			// Voeg de parameters toe
+			if ($stmt->bind_param ( 'iiiissi', $nummer, $id, $productcode, $aantal_besteld, $opmerking, $datum, $bestellingnummer )) {
+				
+				// Voer de query uit
+				if ($stmt->execute ()) {
+					
+				   if ($stmt->affected_rows>0 )	{return true;}
+						
+				    else {return false;}       		
+					
+					
+				} else {
+					// Verwerk errors
+					echo $stmt->error;
+				}
+			} else {
+				// Verwerk errors
+				echo $stmt->error;
+			}
+		} else {
+			// Verwerk errors
+			echo $stmt->error;
+		}
+		
+		// Sluit het statement om geheugen vrij te geven
+	
+		$stmt->close ();
+		
+		
+		
+	}
 }
