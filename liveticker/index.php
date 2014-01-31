18e7b7d77390cde5c78daf8b4ac145087d5427c0
Martijn-96 <martijn_de_m@live.nl>
2014-01-31 10:09:23 +0100

    Update index.php

--- liveticker/index.php
+++ liveticker/index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html > 
+<html>
+<head>
+<link href="opmaak.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>liveticker</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="liveticker.js"></script>
+</head>
+<body>
+<h1> Liveticker </h1>
+<div id='liveticker'>
+<table>
 <?php
+
+require_once('../classes/DatabaseHandler.class.php');
+$db=new DatabaseHandler(); 
+$resultaat=$db->alle_bestellingen(0);
+foreach($resultaat as $bestelling=>$data)
+  {
+  echo '<tr><td>'.$data['nummer'].'</td><td>'.$data['bestellingnummer'].'</td><td>'.$data['id'].'</td><td>'.$data['productcode'].'</td><td>'.$data['aantal_besteld'].'</td><td>'.$data['opmerking'].'</td><td>'.$data['datum'].'</td><td>'.$data['status'].'</td></tr>';
+  }
+?>
+</table>
+</div>
+</body>
+</html>
