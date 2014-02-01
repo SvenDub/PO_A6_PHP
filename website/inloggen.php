9e1b6a8f1559a7623bb6a7e444646b4362cf0253
Martijn-96 <martijn_de_m@live.nl>
2014-02-01 11:28:03 +0100

    inlogpagina

---
+++ website/inloggen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html > 
+<html>
+<head>
+<link href="opmaak.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Inloggen</title>
+</head>
+<body>
+<h1> Inloggen</h1>
+
+
+<form name="inloggen" method="post" action="controleerlogin.php">
+Gebruikersnaam:
+<input name="gebruikersnaam" type="text" id="gebruikersnaam">
+<br />
+Wachtwoord:    
+<input name="wachtwoord" type="password" id="wachtwoord">
+<br />
+<input type="submit" name="Submit" value="Inloggen">
+</form>
+<!-- met een formulier wordt de gebruikersnaam en wachtwoord gevraagd om te kunnen inloggen -->
+
+</body>
+</html>
