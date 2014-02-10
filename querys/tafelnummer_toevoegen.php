6aed9a8b78cd21bca40186f0ca06a4dc6bad4ff4
Martijn-96 <martijn_de_m@live.nl>
2014-02-10 12:48:12 +0100

    Create tafelnummer_toevoegen.php

---
+++ querys/tafelnummer_toevoegen.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('classes/DatabaseHandler.class.php');
+$db=new DatabaseHandler(); 
+
+$db->login(); 
+
+$tafelnummer=$_POST["tafelnummer"];
+
+$resultaat=$db->product_toevoegen($tafelnummer)
+
+if ( $resultaat == true ) {
+  print ("Tafelnummer is toegevoegd ");
+}
+else {print ("Het is niet gelukt om het tafelnummer toe te voegen");}
+?>
