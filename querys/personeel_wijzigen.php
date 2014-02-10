6e6597ac5afe9cfd456a7bc33b12e9ecc8f48c25
Martijn-96 <martijn_de_m@live.nl>
2014-02-10 12:55:17 +0100

    Create personeel_wijzigen.php

---
+++ querys/personeel_wijzigen.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('classes/DatabaseHandler.class.php');
+$db=new DatabaseHandler(); 
+
+$db->login(); 
+
+$id=$_POST["id"];
+$gebruikersnaam=$_POST["gebruikersnaam"];
+$wachtwoord=$_POST["wachtwoord"];
+$beheer=$_POST["beheer"];
+$actief=$_POST["actief"];
+
+$resultaat=$db->personeel_wijzigen($id, $gebruikersnaam, $wachtwoord, $beheer, $actief);
+
+if ( $resultaat == true ) {
+  print ("Personeel succesvol gewijzigd");
+}
+else {print ("Het is niet gelukt om personeel te wijzigen");}
+?>
