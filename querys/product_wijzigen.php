e030717f93111f0459d23f6f6606906de26d4ad7
Martijn-96 <martijn_de_m@live.nl>
2014-02-10 12:57:29 +0100

    Create product_wijzigen.php

---
+++ querys/product_wijzigen.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('classes/DatabaseHandler.class.php');
+$db=new DatabaseHandler(); 
+
+$db->login(); 
+
+$productcode=$_POST["productcode"]
+$categorienummer=$_POST["categorienummer"];
+$gerecht=$_POST["gerecht"];
+$prijs=$_POST["prijs"];
+$actief=$_POST["actief"];
+
+$resultaat=$db->product_wijzigen($productcode, $categorie, $gerecht, $prijs, $actief);
+
+if ( $resultaat == true ) {
+  print ("Product succesvol gewijzigd");
+}
+else {print ("Het is niet gelukt om het product te wijzigen");}
+?>
