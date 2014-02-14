98a83e263b9bdfae5a1c5c6375e3d5686a641556
Martijn-96 <martijn_de_m@live.nl>
2014-02-14 09:15:05 +0100

    beheerscript java

--- database_aanpassen.php
+++ database_aanpassen.php
@@ -12,6 +12,7 @@ $db->login();
 <link href="opmaak.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="beheer.js"></script>
 <title>Beheer database</title>
 </head>
 <body>
