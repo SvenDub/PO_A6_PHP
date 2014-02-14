bbc45d2d197ece4b6ffd8678e6470305beb8f81d
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-02-14 09:39:56 +0100

    Beheer javascript

--- beheer.js
+++ beheer.js
@@ -3,7 +3,11 @@ function opslaan(id) {
 	case 'product_toevoegen':
 		var $inputs = $('#product_toevoegen :input');
 		
-		$.post('querys/product_toevoegen.php', { categorienummer:$inputs['categorienummer'], gerecht: $inputs['gerecht'], prijs: $inputs['prijs'], actief: $inputs['actief'] });
+		if (($inputs['categorienummer'] != '') && ($inputs['gerecht'] != '') && ($inputs['prijs'] != '') && ($inputs['actief'] != '')) {
+			$.post('querys/product_toevoegen.php', { categorienummer:$inputs['categorienummer'], gerecht: $inputs['gerecht'], prijs: $inputs['prijs'], actief: $inputs['actief'] });
+		} else {
+			alert('Voer alle velden in.');
+		}
 		break;
 	case 'tafelnummer_toevoegen':
 		break;
