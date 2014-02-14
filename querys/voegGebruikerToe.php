235b43b514d2972c7ac61d9841e34446ae58c5d8
Martijn-96 <martijn_de_m@live.nl>
2014-02-14 09:04:34 +0100

    Create voegGebruikerToe.php

---
+++ querys/voegGebruikerToe.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('classes/DatabaseHandler.class.php');
+$db=new DatabaseHandler(); 
+
+$db->login(); 
+$gebruikersnaam=$_POST["gebruikersnaam"];
+$wachtwoord=$_POST["wachtwoord"];
+$beheer=$_POST["beheer"];
+$actief=$_POST["actief"];
+
+$resultaat=$db->tafelnummer_toevoegen($gebruikersnaam, $wachtwoord, $beheer, $actief);
+
+if ( $resultaat == true ) {
+  print ("Gebruiker is toegevoegd. ");
+}
+else {print ("Het is niet gelukt om de gebruiker toe te voegen.");}
+?>
