fc419175fc1360a5b39edfc4a083298d43b971d1
Martijn-96 <martijn_de_m@live.nl>
2014-02-14 08:58:13 +0100

    Update tafelnummer_wijzigen.php

--- querys/tafelnummer_wijzigen.php
+++ querys/tafelnummer_wijzigen.php
@@ -4,4 +4,13 @@ require_once('classes/DatabaseHandler.class.php');
 $db=new DatabaseHandler(); 
 
 $db->login(); 
+$tafelnummeroud=$_POST["tafelnummeroud"];
+$tafelnummernieuw=$_POST["tafelnummernieuw"];
 
+$resultaat=$db->tafelnummer_toevoegen($tafelnummeroud, $tafelnummernieuw);
+
+if ( $resultaat == true ) {
+  print ("Tafelnummer is gewijzigd ");
+}
+else {print ("Het is niet gelukt om het tafelnummer te wijzigen");}
+?>
