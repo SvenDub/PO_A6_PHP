257fd02539b005483e665f56bb9bf88defbe4eed
Martijn-96 <martijn_de_m@live.nl>
2014-02-21 11:30:43 +0100

    commentaar

--- querys/product_toevoegen.php
+++ querys/product_toevoegen.php
@@ -3,7 +3,9 @@ require_once ('classes/DatabaseHandler.class.php');
 $db = new DatabaseHandler ();
 
 $db->login ();
-
+/* De databasehandlerclass wordt aangeroepen, daarna wordt doormiddel van deze databasehandlerclass het loginscript 
+opgehaald. Er wordt daarmee gecontroleerd of een gebruiker is ingelogd, anders kan de gebruiker deze pagina niet bekijken.
+*/
 $categorienummer = $_POST ["categorienummer"];
 $gerecht = $_POST ["gerecht"];
 $prijs = $_POST ["prijs"];
@@ -16,6 +18,10 @@ if ($resultaat == true) {
 } else {
 	print ("Het is niet gelukt om het product toe te voegen") ;
 }
+
+/* Via de $_POST worden de variabelen uit het formulier verstuurd naar deze pagina. Hier worden de variabelen vervolgens
+opgehaald en gekoppeld aan een nieuwe variabelen. Deze nieuwe variabelen worden in de query geplaatst. Deze query wordt
+vervolgens uitgevoerd. In de If Else constructie wordt daarna weergegeven of de opdracht geslaagd of mislukt is.*/
 ?>
 
 
