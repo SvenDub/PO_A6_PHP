56bfad110491b3a284abfe0ed161459252f4b36c
Martijn-96 <martijn_de_m@live.nl>
2014-02-21 11:23:00 +0100

    commentaar

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ require_once ('classes/DatabaseHandler.class.php');
 $db = new DatabaseHandler ();
 
 $db->login ();
-
+/* De databasehandlerclass wordt aangeroepen, daarna wordt doormiddel van deze databasehandlerclass het loginscript 
+opgehaald. Er wordt daarmee gecontroleerd of een gebruiker is ingelogd, anders kan de gebruiker deze pagina niet bekijken.
+*/
 ?>
 <html>
 <head>
