abb2266fe23920669488a02f7d0f38361d7d0b00
Martijn-96 <martijn_de_m@live.nl>
2014-02-21 11:33:06 +0100

    commentaar

--- querys/voegGebruikerToe.php
+++ querys/voegGebruikerToe.php
@@ -3,6 +3,9 @@ require_once ('classes/DatabaseHandler.class.php');
 $db = new DatabaseHandler ();
 
 $db->login ();
+/* De databasehandlerclass wordt aangeroepen, daarna wordt doormiddel van deze databasehandlerclass het loginscript 
+opgehaald. Er wordt daarmee gecontroleerd of een gebruiker is ingelogd, anders kan de gebruiker deze pagina niet bekijken.
+*/
 $gebruikersnaam = $_POST ["gebruikersnaam"];
 $wachtwoord = $_POST ["wachtwoord"];
 $beheer = $_POST ["beheer"];
@@ -15,4 +18,7 @@ if ($resultaat == true) {
 } else {
 	print ("Het is niet gelukt om de gebruiker toe te voegen.") ;
 }
+/* Via de $_POST worden de variabelen uit het formulier verstuurd naar deze pagina. Hier worden de variabelen vervolgens
+opgehaald en gekoppeld aan een nieuwe variabelen. Deze nieuwe variabelen worden in de query geplaatst. Deze query wordt
+vervolgens uitgevoerd. In de If Else constructie wordt daarna weergegeven of de opdracht geslaagd of mislukt is.*/
 ?>
