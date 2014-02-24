a30c7fc9be88dd67b11444663f069362b263f1a4
Martijn-96 <martijn_de_m@live.nl>
2014-02-24 12:55:43 +0100

    klanten per periode

--- statistiek.php
+++ statistiek.php
@@ -7,3 +7,28 @@ $db->login ();
 opgehaald. Er wordt daarmee gecontroleerd of een gebruiker is ingelogd, anders kan de gebruiker deze pagina niet bekijken.
 */
 ?>
+<!DOCTYPE html >
+<html>
+<head>
+<link href="opmaak.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Statistieken</title>
+</head>
+<body>
+<h1>Klanten per periode</h1>
+<table>
+<?php
+$klanten_3maanden=$db->klantenPerPeriode(date("Y-m-d"),date("Y-m-d",time()-8035200));
+$klanten_6maanden=$db->klantenPerPeriode(date("Y-m-d",time()-8035200),date("Y-m-d",time()-16070400) );
+$klanten_9maanden=$db->klantenPerPeriode(date("Y-m-d",time()-16070400),date("Y-m-d",time()-24105600) );
+$klanten_12maanden=$db->klantenPerPeriode(date("Y-m-d",time()-24105600),date("Y-m-d",time()-32140800) );
+$max=max($klanten_3maanden,$klanten_6maanden,$klanten_9maanden,$klanten_12maanden);
+$breedte=300/$max;
+print("<tr><td>afgelopen drie maanden</td><td>$klanten_3maanden</td><td><img src='blok.jpg' width='$breedte*$klanten_3maanden' height='40'></td></tr>");
+print("<tr><td>maand 3 tot 6 geleden</td><td>$klanten_6maanden</td><td><img src='blok.jpg' width='$breedte*$klanten_6maanden' height='40'></td></tr>");
+print("<tr><td>maand 6 tot 9 geleden</td><td>$klanten_9maanden</td><td><img src='blok.jpg' width='$breedte*$klanten_9maanden' height='40'></td></tr>");
+print("<tr><td>maand 9 tot 12 geleden</td><td>$klanten_12maanden</td><td><img src='blok.jpg' width='$breedte*$klanten_12maanden' height='40'></td></tr>");
+?>
+</table>
+</body>
+</html>
