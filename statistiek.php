a9c429605dcbc6f232fb4ee8ac8a09844b104e3a
Martijn-96 <martijn_de_m@live.nl>
2014-02-24 12:58:59 +0100

    omzet per periode

--- statistiek.php
+++ statistiek.php
@@ -30,5 +30,20 @@ print("<tr><td>maand 6 tot 9 geleden</td><td>$klanten_9maanden</td><td><img src=
 print("<tr><td>maand 9 tot 12 geleden</td><td>$klanten_12maanden</td><td><img src='blok.jpg' width='$breedte*$klanten_12maanden' height='40'></td></tr>");
 ?>
 </table>
+<h1>Omzet per periode</h1>
+<table>
+<?php
+$omzet_3maanden=$db->omzetPerPeriode(date("Y-m-d"),date("Y-m-d",time()-8035200));
+$omzet_6maanden=$db->omzetPerPeriode(date("Y-m-d",time()-8035200),date("Y-m-d",time()-16070400) );
+$omzet_9maanden=$db->omzetPerPeriode(date("Y-m-d",time()-16070400),date("Y-m-d",time()-24105600) );
+$omzet_12maanden=$db->omzetPerPeriode(date("Y-m-d",time()-24105600),date("Y-m-d",time()-32140800) );
+$max=max($omzet_3maanden,$omzet_6maanden,$omzet_9maanden,$omzet_12maanden);
+$breedte=300/$max;
+print("<tr><td>afgelopen drie maanden</td><td>$omzet_3maanden</td><td><img src='blok.jpg' width='$breedte*$omzet_3maanden' height='40'></td></tr>");
+print("<tr><td>maand 3 tot 6 geleden</td><td>$omzet_3maanden</td><td><img src='blok.jpg' width='$breedte*$omzet_6maanden' height='40'></td></tr>");
+print("<tr><td>maand 6 tot 9 geleden</td><td>$omzet_3maanden</td><td><img src='blok.jpg' width='$breedte*$omzet_9maanden' height='40'></td></tr>");
+print("<tr><td>maand 9 tot 12 geleden</td><td>$omzet_3maanden</td><td><img src='blok.jpg' width='$breedte*$omzet_12maanden' height='40'></td></tr>");
+?>
+</table>
 </body>
 </html>
