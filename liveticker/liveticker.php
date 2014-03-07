4cbaa6c0da241c4e075ab927d7b85d02f710d00f
Martijn-96 <martijn_de_m@live.nl>
2014-03-07 09:13:06 +0100

    Update liveticker.php

--- liveticker/liveticker.php
+++ liveticker/liveticker.php
@@ -14,7 +14,7 @@
 	$db = new DatabaseHandler ();
 	$resultaat = $db->alle_bestellingen ( 0 );
 	foreach ( $resultaat as $bestelling => $data ) {
-		echo '<tr><td>' . $data ['nummer'] . '</td><td>' . $data ['bestellingnummer'] . '</td><td>' . $data ['id'] . '</td><td>' . $data ['productcode'] . '</td><td>' . $data ['aantal_besteld'] . '</td><td>' . $data ['opmerking'] . '</td><td>' . $data ['datum'] . '</td><td>' . $data ['status'] . '</td></tr>';
+		echo '<tr><td>' . $data ['nummer'] . '</td><td>' . $data ['bestellingnummer'] . '</td><td>' . $data ['id'] . '</td><td>' . $data ['productcode'] . '</td><td>' . $data ['aantal_besteld'] . '</td><td>' . $data ['opmerking'] . '</td><td>' . $data ['datum'] . '</td><td>' . $data ['status'] . '</td><td> <button onclick="versturen('.$data['bestellingnummer'].')">Product kan geleverd worden aan de klant</button>   </td></tr>';
 	}
 	
 	?>
