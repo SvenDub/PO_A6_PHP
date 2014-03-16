a117ddcd8171684856dfec404524694e2a008d28
Sven Dubbeld <sven.dubbeld1@gmail.com>
2014-03-16 19:37:07 +0100

    JavaScript commentaar

--- liveticker/liveticker.js
+++ liveticker/liveticker.js
@@ -1,18 +1,22 @@
+var execute;
+
+// Begin de 'refresh cycle' zodra de pagina laadt
 window.onload = function() {
-	refresh();
+	// Start de cycle met 3000ms interval
+	execute = setInterval("refresh()", 3000);
 };
 
+// Haal de tabel met bestellingen op
 refresh = function() {
 	$("#liveticker").load("liveticker.php");
-	setTimeout('refresh()', 3000);
 };
 
-versturen = function( nr ) 
-	{
+// Zet een bestelling klaar om opgehaald te worden
+versturen = function(nr) {
 	$.post('livetickerquery.php', {
 		bestellingnummer : nr
-			}, function(data) {
-				alert(data);
-			});
-	
-	}
+	}, function(data) {
+		refresh();
+	});
+
+};
